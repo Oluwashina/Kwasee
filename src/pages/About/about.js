fdf59734254de294a3d6645f32ed25610b21b304
Oluwashina <shinzbaba@gmail.com>
2022-09-20 11:21:57 +0100

    gallery page added

--- src/pages/About/about.js
+++ src/pages/About/about.js
@@ -13,16 +13,16 @@ const AboutPage = () => {
                         <p className='text-[#262220] opacity-[.75] font-medium tracking-wide'>WELCOME TO KWASEE</p>
                         <h5 className='text-[#262220] font-semibold text-4xl mt-2 max-w-xs leading-[140%] tracking-wide'>THE JOURNEY BEGINS</h5>
                        <div className='max-w-xl'>
-                            <p className='text-[#000000] text-sm tracking-wide mt-4'>
+                            <p className='text-[#000000] text-sm tracking-wide mt-6 leading-6'>
                             Kwasee, a name curled from the rich origin of Benue State in Nigeria which means woman, is a fashion styling Company with certified styling experience, ground breaking records and a broad fashion knowledge that empowers women to become the best version of themselves through their clothing choices - one outfit at a time.
                             </p>
-                            <p className='text-[#000000] text-sm tracking-wide mt-4'>
+                            <p className='text-[#000000] text-sm tracking-wide mt-6 leading-6'>
                             Since 2014, Kwasee has been offering styling services to women all over the world. Looking good and feeling good are two different things and at Kwasee, we help you achieve both.
                             </p>
-                            <p className='text-[#000000] text-sm tracking-wide mt-4'>
+                            <p className='text-[#000000] text-sm tracking-wide mt-6 leading-6'>
                             We also help you discover a signature style that suits your personality and body type. It is not whether you or not you have a style, is it expressed through your clothes? Use your style as tool stand out.
                             </p>
-                            <p className='text-[#000000] text-sm tracking-wide mt-4'>
+                            <p className='text-[#000000] text-sm tracking-wide mt-6 leading-6'>
                             Our goal is to help you elevate your style journey to the next.
                             </p>
                         </div>
--- src/pages/Gallery/gallery.js
+++ src/pages/Gallery/gallery.js
@@ -1,11 +1,105 @@
 import React from 'react';
-
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
+import img1 from '../../assets/gallery/img1.png'
+import img2 from '../../assets/gallery/img2.png'
+import img3 from '../../assets/gallery/img3.png'
+import img4 from '../../assets/gallery/img4.png'
+import img5 from '../../assets/gallery/img5.png'
+import img6 from '../../assets/gallery/img6.png'
+import img7 from '../../assets/gallery/img7.png'
+import img8 from '../../assets/gallery/img8.png'
+import img9 from '../../assets/gallery/img9.png'
+import img10 from '../../assets/gallery/img10.png'
+import img11 from '../../assets/gallery/img11.png'
+import img12 from '../../assets/gallery/img12.png'
+import img13 from '../../assets/gallery/img13.png'
+import img14 from '../../assets/gallery/img14.png'
+import img15 from '../../assets/gallery/img15.png'
+import img16 from '../../assets/gallery/img16.png'
+import img17 from '../../assets/gallery/img17.png'
+import img18 from '../../assets/gallery/img18.png'
+import img19 from '../../assets/gallery/img19.png'
+import img20 from '../../assets/gallery/img20.png'
+import img21 from '../../assets/gallery/img21.png'
+import img22 from '../../assets/gallery/img22.png'
+import img23 from '../../assets/gallery/img23.png'
+import img24 from '../../assets/gallery/img24.png'
+import img25 from '../../assets/gallery/img25.png'
+import img26 from '../../assets/gallery/img26.png'
+import img27 from '../../assets/gallery/img27.png'
+import img28 from '../../assets/gallery/img28.png'
+import img29 from '../../assets/gallery/img29.png'
+import img30 from '../../assets/gallery/img30.png'
+import img31 from '../../assets/gallery/img31.png'
+import img32 from '../../assets/gallery/img32.png'
+import img33 from '../../assets/gallery/img33.png'
+import img34 from '../../assets/gallery/img34.png'
 
 const GalleryPage = () => {
+
+    const images = [
+      img1,
+      img2,
+      img3,
+      img4,
+      img5,
+      img6,
+      img7,
+      img8,
+      img9,
+      img10,
+      img11,
+      img12,
+      img13,
+      img14,
+      img15,
+      img16,
+      img17,
+      img18,
+      img19,
+      img20,
+      img21,
+      img22,
+      img23,
+      img24,
+      img25,
+      img26,
+      img27,
+      img28,
+      img29,
+      img30,
+      img31,
+      img32,
+      img33,
+      img34,
+      img12,
+      img3,
+      img8,
+      img5,
+      img1,
+      img16,
+      img4,
+      img2
+    ]
+
     return ( 
         <>
             <div>
-                
+                <ResponsiveMasonry
+                    columnsCountBreakPoints={{350: 2, 750: 2, 900: 3}}
+                >
+                    <Masonry 
+                    gutter="10px">
+                        {images.map((image, i) => (
+                            <img
+                                key={i}
+                                src={image}
+                                alt="gallery"
+                                style={{width: "100%", display: "block"}}
+                            />
+                        ))}
+                </Masonry>
+               </ResponsiveMasonry>
             </div>
         </>
      );
