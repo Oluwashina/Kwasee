115c672e1f21b6374d1bf84b5d87cc77c9362e6b
Oluwashina <shinzbaba@gmail.com>
2022-09-21 15:27:18 +0100

    logo and navbar modifications

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import {HashLink as Link} from 'react-router-hash-link'
-import logo from '../assets/icons/logo.svg'
+import logo from '../assets/icons/Kwasee_black.svg'
 import menu from '../assets/icons/menu.svg'
 import close_icon from '../assets/icons/close.svg'
 
@@ -45,8 +45,8 @@ const NavbarComponent = () => {
    
     return ( 
         <>
-         <div className='py-6 md:py-4 md:px-8 px-4'>
-             <div className='text-center'>
+         <div className='py-6 md:py-6 md:px-8 px-4'>
+             <div className='text-center md:hidden'>
                  <p className='text-[#000000] font-medium tracking-wide md:tracking-widest text-xs md:text-sm'>Becoming the best version of yourself, one outfit at a time</p>
              </div>
 
@@ -71,11 +71,15 @@ const NavbarComponent = () => {
                 </li>
              </ul>
 
+             <div className='text-center mt-4 hidden md:block'>
+                 <p className='text-[#000000] font-medium tracking-wide md:tracking-widest text-xs md:text-sm'>Becoming the best version of yourself, one outfit at a time</p>
+             </div>
+
              {/* mobile nav */}
-             <div className={navShow ? 'hidden' : 'md:hidden flex items-center justify-between mt-5'}>
+             <div className={navShow ? 'hidden' : 'md:hidden flex items-center justify-between mt-6'}>
                  <div>
                    <Link to="/" className="text-[#262220] text-sm tracking-widest">
-                        <img src={logo} className="w-[100px]" alt="logo" />    
+                        <img src={logo} className="w-[120px]" alt="logo" />    
                     </Link>
                  </div>
                  <div onClick={toggleOpen} className='cursor-pointer'>
@@ -84,7 +88,7 @@ const NavbarComponent = () => {
              </div>
 
              {/* mobile menu */}
-             <div className={navShow ? 'md:hidden bg-[#D5D5D5] w-full h-full top-0 left-0 z-20 fixed': 'hidden'}>
+             <div className={navShow ? 'md:hidden bg-[#D5D5D5] w-full h-full top-0 left-0 z-20 fixed overflow-hidden': 'hidden'}>
                  <div onClick={toggleClose} className='cursor-pointer absolute right-6 top-6 bg-[#262220] flex justify-center items-center w-[40px] h-[40px] rounded-full'>
                      <img src={close_icon} alt="close" />
                  </div>
