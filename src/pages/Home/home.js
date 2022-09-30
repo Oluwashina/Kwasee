046ab24c047fb4b4b968b1db31f08fc4318c6722
Oluwashina <shinzbaba@gmail.com>
2022-09-30 10:55:44 +0100

    video and our services added

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -9,6 +9,7 @@ import axios from 'axios'
 import pic3 from '../../assets/icons/sho.svg'
 import hero from '../../assets/hero.svg'
 import hero1 from '../../assets/hero1.svg'
+import hero2 from '../../assets/hero2.svg'
 import service1 from '../../assets/T.svg'
 import service2 from '../../assets/S.svg'
 import service3 from '../../assets/Y.svg'
@@ -151,17 +152,17 @@ const HomePage = () => {
                             <img src={hero1} alt="hero" />
                         </div>
                         <div className='absolute bottom-10 md:bottom-20 right-6 md:right-20'>
-                            <h5 className='font-della text-[#F7F1F0] md:text-4xl text-lg md:tracking-widest font-medium max-w-sm mx-auto text-right md:text-center'>It is not whether or <br/> not you have style, </h5>  
+                            <h5 className='font-della text-[#F7F1F0] md:text-4xl text-lg md:tracking-widest font-medium max-w-sm mx-auto text-right md:text-center'>  It is not whether or <br/> not you have style, </h5>  
                             <p className='uppercase text-[#F7F1F0] font-della font-medium text-xs md:text-sm mt-2 md:mt-4'>is it expressed through your clothes? </p>  
                         </div>                   
                     </div>
                     <div className='bg-[#262220] h[400px] md:h-[800px] flex overflow-hidden relative'>
                         <div>
-                            <img src={hero} alt="hero" />
+                            <img src={hero2} alt="hero" />
                         </div>
                         <div className='absolute bottom-10 md:bottom-1/2 left-1/2'>
                             <h5 className='font-della text-[#F7F1F0] md:text-4xl tracking-widest font-medium max-w-sm mx-auto'> Use <br/> Your Style <br/> As A Tool </h5>  
-                            <p className='uppercase text-[#F7F1F0] font-della font-medium text-xs md:text-sm mt-2 md:mt-3'>to stand out</p>  
+                            <p className='uppercase text-[#F7F1F0] font-della font-medium text-xs md:text-sm mt-2 md:mt-4'>to stand out</p>  
                         </div>                   
                     </div>
                 </Slider>
@@ -171,32 +172,32 @@ const HomePage = () => {
             <div className='py-10'>
                 <h6 className='text-[#262220] tracking-widest text-center font-della font-medium text-xl md:text-2xl'>Packages</h6>
                 <div className='grid grid-cols-4 gap-56 md:gap-6 mt-8 px-4 md:px-8 overflow-scroll'>
-                    <div className='bg-virtual_small bg-cover bg-center w-[200px] md:w-auto h-[250px] md:h-[400px] rounded-lg md:rounded-none relative'>
+                    <Link to="/virtual-styling" className='bg-virtual_small bg-cover bg-center w-[200px] cursor-pointer md:w-auto h-[250px] md:h-[400px] rounded-lg md:rounded-none relative'>
 
                         <div className='text-center absolute  px-6 md:px-0 mx-auto left-0 right-0 bottom-14 md:bottom-10'>
                             <h6 className='text-[#F7F1F0] text-base md:text-base font-della font-medium tracking-wide'>VIRTUAL STYLING </h6>
                         </div>
 
-                    </div>
+                    </Link>
 
-                    <div  className='bg-birthday_small bg-cover bg-center w-[200px] md:w-auto h-[250px] md:h-[400px] relative rounded-lg md:rounded-none'>
+                    <Link to="/birthday-shoot" className='bg-birthday_small bg-cover bg-center w-[200px] cursor-pointer md:w-auto h-[250px] md:h-[400px] relative rounded-lg md:rounded-none'>
                       <div className='text-center absolute px-6 md:px-0 mx-auto left-0 right-0 bottom-14 md:bottom-10'>
                             <h6 className='text-[#F7F1F0] font-della text-base md:text-base font-medium tracking-wide'>BIRTHDAY SHOOT </h6>
                         </div>
 
-                    </div>
+                    </Link>
 
-                    <div className='bg-maternity_small bg-cover bg-center] w-[200px] md:w-auto h-[250px]  md:h-[400px] relative rounded-lg md:rounded-none'>
+                    <Link to="/maternity-shoot" className='bg-maternity_small bg-cover bg-center] w-[200px] cursor-pointer md:w-auto h-[250px]  md:h-[400px] relative rounded-lg md:rounded-none'>
                         <div className='text-center absolute px-6 md:px-0 mx-auto left-0 right-0 bottom-10 md:bottom-10'>
                             <h6 className='text-[#F7F1F0] font-della text-base md:text-base font-medium tracking-wide'>MATERNITY SHOOT</h6>
                         </div>
-                    </div>
+                    </Link>
 
-                    <div className='bg-shopping bg-cover bg-center w-[200px] md:w-auto h-[250px]  md:h-[400px] relative rounded-lg md:rounded-none'>
+                    <Link to="/personal-shopping" className='bg-shopping bg-cover bg-center w-[200px] cursor-pointer md:w-auto h-[250px]  md:h-[400px] relative rounded-lg md:rounded-none'>
                         <div className='text-center px-6 md:px-0 absolute mx-auto left-0 right-0 bottom-10 md:bottom-10'>
                             <h6 className='text-[#F7F1F0] font-della text-base md:text-base font-medium tracking-wide'>ASSISTED PHYSICAL <br/> SHOPPING</h6>
                         </div>
-                     </div>
+                     </Link>
 
                 </div>
 
@@ -227,7 +228,21 @@ const HomePage = () => {
                             </div>
                    </Slider>
                    
+                </div>
 
+                <div className='mt-10 md:mt-12'>
+                  {/* <video controls width="100%">
+                    <source src={src} type="video/*" />
+                    Sorry, your browser doesn't support embedded videos.
+                    </video> */}
+
+                <div className=''>
+                    <iframe width="100%" 
+                    height="800" src="https://www.youtube.com/embed/PO_5uiap9Cg" 
+                    title="Commercial Styling" frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen></iframe>
+                </div>
 
                 </div>
 
@@ -256,21 +271,21 @@ const HomePage = () => {
                     <h6 className='text-[#262220] font-della tracking-[0.03em] text-center font-medium text-xl md:text-2xl'>Our Services</h6>
 
                     <div className='mt-4 md:mt-8 grid grid-cols-5'>
-                        <div>
+                        <Link to="/our-services">
                             <img src={service1} alt="service1" />
-                        </div>
-                        <div>
+                        </Link>
+                        <Link to="/our-services">
                             <img src={service2} alt="service2" />
-                        </div>
-                        <div>
+                        </Link>
+                        <Link to="/our-services">
                             <img src={service3} alt="service3" />
-                        </div>
-                        <div>
+                        </Link>
+                        <Link to="/our-services">
                             <img src={service4} alt="service4" />
-                        </div>
-                        <div>
+                        </Link>
+                        <Link to="/our-services">
                             <img src={service5} alt="service5" />
-                        </div>
+                        </Link>
 
                     </div>
                 </div>
