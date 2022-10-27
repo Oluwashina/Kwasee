cf1709e8a143d4d91a1463b63448b4d5db7fab95
Oluwashina <shinzbaba@gmail.com>
2022-10-27 23:02:30 +0100

    modifications made on servicesa page

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -287,14 +287,21 @@ const HomePage = () => {
                     </Link>
 
                     <Link to="/commercial-style" className='group bg-service3 w-[200px] bg-cover bg-center bg-no-repeat  cursor-pointer md:w-auto h-[400px] md:h-[677px] relative rounded-lg md:rounded-none'>
+                        <div className='text-center absolute  px-6 md:px-0 mx-auto left-0 right-0 bottom-10 md:bottom-20'>
+                            <h6 className='text-[#F7F1F0] text-2xl md:text-3xl font-della font-medium'>Commercial <br/> Styling </h6>
+                        </div>
                         <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 md:rounded-none rounded-lg md:group-hover:opacity-[.3]"></div>
                     </Link>
 
                     <Link to="/virtual-style" className='group bg-service4 bg-cover bg-no-repeat bg-center  w-[200px] cursor-pointer md:w-auto h-[400px]  md:h-[677px] relative rounded-lg md:rounded-none'>
+                        
                         <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 md:rounded-none rounded-lg md:group-hover:opacity-[.3]"></div>
                      </Link>
 
                      <Link to="/fashion-consult" className='group bg-service5 bg-cover bg-center bg-no-repeat  w-[200px] cursor-pointer md:w-auto h-[400px]  md:h-[677px] relative rounded-lg md:rounded-none'>
+                         <div className='text-center absolute  px-6 md:px-0 mx-auto left-0 right-0 bottom-10 md:bottom-14'>
+                            <h6 className='text-[#F7F1F0] text-2xl md:text-3xl font-della font-medium'>Fashion <br/> Consultation </h6>
+                        </div>
                         <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 md:rounded-none rounded-lg md:group-hover:opacity-[.3]"></div>
                      </Link>
                     </div>
