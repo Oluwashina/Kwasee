96087a3031303651d58d5f3a03fcd99abc6e457c
Oluwashina <shinzbaba@gmail.com>
2022-10-27 22:08:23 +0100

    modifications made on services page

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -11,11 +11,6 @@ import pic3 from '../../assets/IMG_0158 1.jpg'
 import hero from '../../assets/hero_.png'
 import hero1 from '../../assets/hero1.svg'
 import hero2 from '../../assets/hero2.svg'
-import service1 from '../../assets/T.svg'
-import service2 from '../../assets/S.svg'
-import service3 from '../../assets/Y.svg'
-import service4 from '../../assets/L.svg'
-import service5 from '../../assets/E.svg'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -91,7 +86,7 @@ const HomePage = () => {
 
     useEffect(()=>{
         setTimeout(() => {
-            setShow(true)
+            setShow(false)
         }, 3000);
     },[])
 
@@ -279,28 +274,29 @@ const HomePage = () => {
                 <div className='mt-10 md:mt-12'>
                     <h6 className='text-[#262220] font-della tracking-[0.03em] text-center font-medium text-xl md:text-2xl'>Our Services</h6>
 
-                    <div className='mt-4 md:mt-8 grid grid-cols-5'>
-                        <Link to="/bridal-style" className='relative group'>
-                            <img src={service1} alt="service1" />
-                            <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 group-hover:opacity-[.2]"></div>
-                        </Link>
-                        <Link to="/birthday-style" className='relative group'>
-                            <img src={service2} alt="service2" />
-                            <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 group-hover:opacity-[.2]"></div>
-                        </Link>
-                        <Link to="/commercial-style" className='relative group'>
-                            <img src={service3} alt="service3" />
-                            <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 group-hover:opacity-[.2]"></div>
-                        </Link>
-                        <Link to="/virtual-style" className='relative group'>
-                            <img src={service4} alt="service4" />
-                            <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 group-hover:opacity-[.2]"></div>
-                        </Link>
-                        <Link to="/fashion-consult" className='relative group'>
-                            <img src={service5} alt="service5" />
-                            <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 group-hover:opacity-[.2]"></div>
-                        </Link>
+                    <div className='mt-4 md:mt-8 grid grid-cols-5 gap-56 md:gap-0 mt-8 px-4 md:px-0 overflow-scroll'>
+                    <Link to="/bridal-style" className='group bg-service2 bg-cover bg-center bg-no-repeat w-[200px] cursor-pointer md:w-full h-[400px] md:h-[677px] rounded-lg md:rounded-none relative'>
+                        <div className='text-center absolute  px-6 md:px-0 mx-auto left-0 right-0 bottom-10 md:bottom-14'>
+                            <h6 className='text-[#F7F1F0] text-2xl md:text-3xl font-della font-medium'>Bridal <br/> Styling </h6>
+                        </div>
+                        <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 md:rounded-none rounded-lg md:group-hover:opacity-[.3]"></div>
+                    </Link>
 
+                    <Link to="/birthday-style" className='group bg-service1 bg-cover bg-center bg-no-repeat w-[200px] cursor-pointer md:w-auto h-[400px] md:h-[677px] relative rounded-lg md:rounded-none'>
+                        <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 md:rounded-none rounded-lg md:group-hover:opacity-[.3]"></div>
+                    </Link>
+
+                    <Link to="/commercial-style" className='group bg-service3 w-[200px] bg-cover bg-center bg-no-repeat  cursor-pointer md:w-auto h-[400px] md:h-[677px] relative rounded-lg md:rounded-none'>
+                        <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 md:rounded-none rounded-lg md:group-hover:opacity-[.3]"></div>
+                    </Link>
+
+                    <Link to="/virtual-style" className='group bg-service4 bg-cover bg-no-repeat bg-center  w-[200px] cursor-pointer md:w-auto h-[400px]  md:h-[677px] relative rounded-lg md:rounded-none'>
+                        <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 md:rounded-none rounded-lg md:group-hover:opacity-[.3]"></div>
+                     </Link>
+
+                     <Link to="/fashion-consult" className='group bg-service5 bg-cover bg-center bg-no-repeat  w-[200px] cursor-pointer md:w-auto h-[400px]  md:h-[677px] relative rounded-lg md:rounded-none'>
+                        <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 md:rounded-none rounded-lg md:group-hover:opacity-[.3]"></div>
+                     </Link>
                     </div>
                 </div>
             </div>
--- src/pages/Services/services.js
+++ src/pages/Services/services.js
@@ -1,40 +1,33 @@
 import React from 'react';
-import service1 from '../../assets/T.svg'
-import service2 from '../../assets/S.svg'
-import service3 from '../../assets/Y.svg'
-import service4 from '../../assets/L.svg'
-import service5 from '../../assets/E.svg'
 import { Link } from 'react-router-dom';
 
 const ServicesPage = () => {
     return ( 
         <>
-            <div className='pt-10'>
+            <div className='pt-10 md:pb-0 pb-10'>
                 <h6 className='text-[#262220] tracking-widest font-della text-center font-medium text-2xl'>Our Services</h6>
                
-                <div className='mt-4 md:mt-8 grid grid-cols-5'>
-                        <Link to="/bridal-style" className='relative group'>
-                            <img src={service1} alt="service1" />
-                            <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 group-hover:opacity-[.2]"></div>
-                        </Link>
-                        <Link to="/birthday-style" className='relative group'>
-                            <img src={service2} alt="service2" />
-                            <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 group-hover:opacity-[.2]"></div>
-                        </Link>
-                        <Link to="/commercial-style" className='relative group'>
-                            <img src={service3} alt="service3" />
-                            <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 group-hover:opacity-[.2]"></div>
-                        </Link>
-                        <Link to="/virtual-style" className='relative group'>
-                            <img src={service4} alt="service4" />
-                            <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 group-hover:opacity-[.2]"></div>
-                        </Link>
-                        <Link to="/fashion-consult" className='relative group'>
-                            <img src={service5} alt="service5" />
-                            <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 group-hover:opacity-[.2]"></div>
-                        </Link>
+                <div className='mt-4 md:mt-8 grid grid-cols-5 gap-56 md:gap-0 mt-8 px-4 md:px-0 overflow-scroll'>
+                    <Link to="/bridal-style" className='group bg-service2 bg-cover bg-no-repeat w-[200px] cursor-pointer md:w-full h-[400px] md:h-[677px] rounded-lg md:rounded-none relative'>
+                        <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 md:rounded-none rounded-lg md:group-hover:opacity-[.3]"></div>
+                    </Link>
 
-                 </div>
+                    <Link to="/birthday-style" className='group bg-service1 bg-cover bg-no-repeat  w-[200px] cursor-pointer md:w-auto h-[400px] md:h-[677px] relative rounded-lg md:rounded-none'>
+                        <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 md:rounded-none rounded-lg md:group-hover:opacity-[.3]"></div>
+                    </Link>
+
+                    <Link to="/commercial-style" className='group bg-service3 w-[200px] bg-cover bg-no-repeat  cursor-pointer md:w-auto h-[400px] md:h-[677px] relative rounded-lg md:rounded-none'>
+                        <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 md:rounded-none rounded-lg md:group-hover:opacity-[.3]"></div>
+                    </Link>
+
+                    <Link to="/virtual-style" className='group bg-service4 bg-cover bg-no-repeat  w-[200px] cursor-pointer md:w-auto h-[400px]  md:h-[677px] relative rounded-lg md:rounded-none'>
+                        <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 md:rounded-none rounded-lg md:group-hover:opacity-[.3]"></div>
+                     </Link>
+
+                     <Link to="/fashion-consult" className='group bg-service5 bg-cover bg-no-repeat  w-[200px] cursor-pointer md:w-auto h-[400px]  md:h-[677px] relative rounded-lg md:rounded-none'>
+                        <div class="absolute inset-0 w-full h-full transition-all bg-[#000] opacity-0 md:rounded-none rounded-lg md:group-hover:opacity-[.3]"></div>
+                     </Link>
+                    </div>
             </div>
         </>
      );
